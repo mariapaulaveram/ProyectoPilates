cbc1f58698d6c4cc2cf554dd7e50a24a0fcfc289
mariapaulavera <mariapaulaveram@gmail.com>
2025-12-13 13:05:37 -0300

    mejoro la parte responsive

--- front-pilates/src/componentes/layouts/Nav.jsx
+++ front-pilates/src/componentes/layouts/Nav.jsx
@@ -1,22 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/Nav.module.css";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navContent}>
         <div className={styles.logoGroup}>
-          <Link to="/" className={styles.logo}>
-             {/* <img src="../../public/logo.jpg" alt="Pilates" /> */}
+          <Link to="/" className={styles.logo} onClick={() => setOpen(false)}>
             <img src="/logo.jpg" alt="Pilates" />
           </Link>
           <h2 className={styles.logoTitle}>Pilates Studio</h2>
         </div>
-        <ul className={styles.links}>
-          <li><Link to="/#pilates-home">Inicio</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/registro">Registrarse</Link></li>
-          <li><Link to="/#horarios">Horarios</Link></li>
+
+        {/* Botón hamburguesa */}
+        <button
+          className={styles.menuToggle}
+          onClick={() => setOpen(!open)}
+          aria-expanded={open}
+          aria-label="Abrir menú de navegación"
+        >
+          ☰
+        </button>
+
+        {/* Links */}
+        <ul className={`${styles.links} ${open ? styles.show : ""}`}>
+          <li><Link to="/#pilates-home" onClick={() => setOpen(false)}>Inicio</Link></li>
+          <li><Link to="/login" onClick={() => setOpen(false)}>Login</Link></li>
+          <li><Link to="/registro" onClick={() => setOpen(false)}>Registrarse</Link></li>
+          <li><Link to="/#horarios" onClick={() => setOpen(false)}>Horarios</Link></li>
         </ul>
       </div>
     </nav>
@@ -24,9 +38,6 @@ function Navbar() {
 }
 
 export default Navbar;
-
-
-
 
 
 
